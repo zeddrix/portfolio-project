9891de5fbbe38987a149434acb9033597347c1a1
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-03-13 15:33:50 +0800

    Created Projects model.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hobby.apps.HobbyConfig',
+    'projects.apps.ProjectsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
