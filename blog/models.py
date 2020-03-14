0a32d7865b1fbfe1178d72c777918fff3a3e4f22
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-03-14 12:37:15 +0800

    Created Blog model. Published 3 blog posts.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    pub_date = models.DateField(auto_now_add=True)
+    # publication date
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+    body = models.TextField()
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'hobby.apps.HobbyConfig',
     'projects.apps.ProjectsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
