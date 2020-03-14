02742951292147387c5867a39aa28b791cb06e14
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-03-14 17:35:10 +0800

    Created homepage and a new page for projects.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import hobby.views
+import projects.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('projects/', projects.views.projects, name='projects'),
+    path('', hobby.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def projects(request):
+    return render(request, 'projects/projects.html')
