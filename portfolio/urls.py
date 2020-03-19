4cc63caddeee8b87df3f455ec443af62161eeaa9
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-03-20 06:58:56 +0800

    Successfully created the blog page.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import hobby.views
@@ -23,5 +23,6 @@ import projects.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', projects.views.projects, name='projects'),
+    path('blog/', include('blog.urls')),
     path('', hobby.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
